fe2dd4db24ea1ace6351a4ec4988c07b7690f33c
pmakin-png <priyanka.makin@sparkfun.com>
2020-03-24 10:54:04 -0600

    Updated batch file and .hex
    Batch file now flashes bootloader and firmware correctly

--- Firmware/Qwiic_Step/program_qwiic_step.bat
+++ Firmware/Qwiic_Step/program_qwiic_step.bat
@@ -2,14 +2,8 @@
 @pause
 :loop
 
-@echo - 
-@echo Flashing bootloader...
-@avrdude -C avrdude.conf -v -patmega328p -cusbtiny -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m 
-
-@timeout 1
-
-@echo Flashing firmware...
-@avrdude -C avrdude.conf -pm328p -cusbtiny -e -Uflash:w:Qwiic_Step.ino.standard.hex:i
+@echo Flashing bootloader and  firmware...
+@avrdude -C avrdude.conf -pm328p -cusbtiny -e -Uflash:w:Qwiic_Step.ino.with_bootloader.standard.hex:i
 
 @echo Done programming! Move on to the next board.
 @pause
