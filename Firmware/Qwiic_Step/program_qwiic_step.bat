f312221112c0b1f88038292d29e04243cf996dfb
pmakin-png <priyanka.makin@sparkfun.com>
2020-08-04 12:41:07 -0600

    Update program_qwiic_step.bat
    Add fuse bits

--- Firmware/Qwiic_Step/program_qwiic_step.bat
+++ Firmware/Qwiic_Step/program_qwiic_step.bat
@@ -3,7 +3,7 @@
 :loop
 
 @echo Flashing bootloader and  firmware...
-@avrdude -C avrdude.conf -pm328p -cusbtiny -e -Uflash:w:Qwiic_Step.ino.with_bootloader.standard.hex:i
+@avrdude -C avrdude.conf -pm328p -cusbtiny -e -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m -Uflash:w:Qwiic_Step.ino.with_bootloader.standard.hex:i
 
 @echo Done programming! Move on to the next board.
 @pause
